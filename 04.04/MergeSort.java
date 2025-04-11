a5f1f7a726d8860b2f9d70d6433db8fa65e61b41
PalmaDae <arturd277@mail.ru>
2025-04-11 11:04:26 +0300

    new

--- 04.04/MergeSort.java
+++ 04.04/MergeSort.java
@@ -1,17 +1,22 @@
 public class MergeSort {
 
-    public void sort(int[] arr) {
+	public void sort(int[] arr) {
         if (arr.length < 2) {
             return;
         }
 
         int mid = arr.length / 2;
+        
         int[] left = new int[mid];
         int[] right = new int[arr.length - mid];
-
-		
-        System.arraycopy(arr, 0, left, 0, mid);
-        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+        
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        
+        for (int i = mid; i < arr.length; i++) {
+            right[i - mid] = arr[i];
+        }
 
         sort(left);
         sort(right);
@@ -19,23 +24,23 @@ public class MergeSort {
         merge(arr, left, right);
     }
 
-	private void merge(int[] arr, int[] left, int[] right) {
+	private void merge(int[] array, int[] left, int[] right) {
 		int i = 0, j = 0, k = 0;
 
 		while (i < left.length && j < right.length) {
 			if (left[i] <= right[j]) {
-				arr[k++] = left[i++];
+				array[k++] = left[i++];
 			} else {
-				arr[k++] = right[j++];
+				array[k++] = right[j++];
 			}
 		}
 
 		while (i < left.length) {
-			arr[k++] = left[i++];
+			array[k++] = left[i++];
 		}
 
 		while (j < right.length) {
-			arr[k++] = right[j++];
+			array[k++] = right[j++];
 		}
 	}
 }
