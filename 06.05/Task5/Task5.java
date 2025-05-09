f6cd974918787a6856989d36fc4a20301405b85d
PalmaDae <arturd277@mail.ru>
2025-05-09 20:19:05 +0300

    BrainRot Exception

--- 06.05/Task5/Task5.java
+++ 06.05/Task5/Task5.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Task5 {
 	@SuppressWarnings("deprecation")
@@ -14,9 +15,10 @@ public class Task5 {
 			fos.write(i);
 		}
 		fos.close();
+		is.close();
 	}
 	
-	public static void parsePictures(String http) throws IOException{
+	public static void picturesInNotepad(String http) throws IOException{
 		BufferedReader reader = new BufferedReader(new FileReader(http));
 		PrintWriter writer = new PrintWriter(new FileWriter("C:\\Java_Projects\\06.05\\Task5\\Files\\src.txt"));
 		
@@ -24,7 +26,7 @@ public class Task5 {
 		
 		while((line = reader.readLine()) != null) {
 			line = line.toLowerCase();
-			if (line.contains("<img") && (line.contains(".jpg") || line.contains(".png"))) {
+			if (line.contains("<img") && line.contains("https") && (line.contains(".jpg") || line.contains(".png"))) {
 				
 				String[] data = line.split("src=\"");
 				for (int i = 1; i < data.length; i++) {
@@ -35,13 +37,28 @@ public class Task5 {
 		}
 	}
 	
+	public static void parsePictures(String src) throws IOException{
+		String output = "C:\\Java_Projects\\06.05\\Task5\\Images\\picture";
+		Scanner scanner = new Scanner(new File(src));
+		
+		int i = 0;
+		
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			httpFromUrl(line, (output + i + ".jpg"));
+			i++;
+		}
+	}
+	
 	public static void main(String[] args) {
 		String urlByString = "https://kpfu.ru/itis";
 		String http = "C:\\Java_Projects\\06.05\\Task5\\Files\\HTTP.txt";
+		String src = "C:\\Java_Projects\\06.05\\Task5\\Files\\src.txt";
 		
 		try {
 			httpFromUrl(urlByString, http);
-			parsePictures(http);
+			picturesInNotepad(http);
+			parsePictures(src);
 		} catch(IOException e) {
 			System.out.println("");
 		}
